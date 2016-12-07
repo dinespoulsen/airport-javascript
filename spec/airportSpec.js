0763961341a5a855df53833c1dccb55672e0eaab
Dines Poulsen <dinespoulsen84@gmail.com>
2016-12-07 10:46:38 +0000

    Added stubbing to weather

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -2,12 +2,14 @@
 
 describe ('Airport', function() {
 
-	var airport
-	var plane
+	var airport;
+	var plane;
+	var weather;
 
 	beforeEach(function() {
 		airport = new Airport();
 		plane = jasmine.createSpyObj('plane', ['land', 'takeOff']);
+		weather = jasmine.createSpyObj('weather', ['isStormy']);
 	});
 
 	it("should be able to instruct a plane to land", function() {
@@ -26,6 +28,20 @@ describe ('Airport', function() {
 	});
 
 	it("no longer holds a plane after take off", function() {
+		airport.land(plane);
+		airport.takeOff(plane);
+		spyOn(airport, 'isStormy').and.returnValue(false);
+		expect(airport.planes).not.toContain(plane);
+	});
+
+	it("should prevent take off when weather is stormy", function(){
+		spyOn(airport, 'isStormy').and.returnValue(true);
+		airport.land(plane);
+		expect( function() {airport.takeOff(plane); }).toThrow(new Error("Plane cannot take off: weather is too stormy"))
+	});
+
+	it("should allow take off when weather is not stormy", function(){
+		spyOn(airport, 'isStormy').and.returnValue(false);
 		airport.land(plane);
 		airport.takeOff(plane);
 		expect(airport.planes).not.toContain(plane);
--- src/airport.js
+++ src/airport.js
@@ -7,8 +7,17 @@ Airport.prototype.land = function(plane) {
 	this.planes.push(plane);
 };
 
+Airport.prototype.isStormy = function(){
+	return false;
+};
+
 Airport.prototype.takeOff = function(plane) {
-	plane.takeOff();
-	var indexPlane = this.planes.indexOf(plane)
-	delete this.planes[indexPlane];
+	if (this.isStormy()){
+		throw new Error("Plane cannot take off: weather is too stormy");
+	}
+	else {
+		plane.takeOff();
+		var indexPlane = this.planes.indexOf(plane)
+		delete this.planes[indexPlane];
+	}
 };
